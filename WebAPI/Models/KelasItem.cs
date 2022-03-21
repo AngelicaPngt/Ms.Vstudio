2e8faec8918d71d0f6f7551a962a1defee30b153
AngelicaPngt <nglcpanget@gmail.com>
2022-03-22 00:24:09 +0700

    first commit

---
+++ WebAPI/Models/KelasItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class KelasItem
+    {
+        private KelasContext _context;
+        public int id { get; set; }
+        public string kelas { get; set; }
+        public string jurusan { get; set; }
+        public int sub { get; set; }
+    }
+}
